a7b151764a158db18de05a86b286001ff78f2790
Cristiano <cristiano.988@hotmail.com>
2017-08-16 23:03:10 +0100

    Commit 2

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+
         //Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("settings.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setMaximized(true);
